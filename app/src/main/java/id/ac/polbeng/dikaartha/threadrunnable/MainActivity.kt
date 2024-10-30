ccb038c13c3290585f50015dc7c09d5a04bb6941
MSI GAMING <arthdika17@gmail.com>
2024-10-30 21:37:21 +0700

    add

--- app/src/main/java/id/ac/polbeng/dikaartha/threadrunnable/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/dikaartha/threadrunnable/MainActivity.kt
@@ -1,28 +1,38 @@
 package id.ac.polbeng.dikaartha.threadrunnable
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.ac.polbeng.dikaartha.threadrunnable.databinding.ActivityMainBinding
+import android.os.Handler
+import android.os.Message
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mHandler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mHandler = MyHandler()
         binding.button.setOnClickListener {
-            val runnable = Worker()
-            val thread = Thread(runnable)
-            thread.start()
+            Thread {
+                killSomeTime()
+            }.start()
         }
     }
-    inner class Worker : Runnable {
-        override fun run() {
-            killSomeTime()
+    @SuppressLint("HandlerLeak")
+    inner class MyHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            binding.textView.text = msg.data?.getString("counter")
         }
     }
     private fun killSomeTime() {
         for (i in 1..20) {
+            val msg = Message.obtain()
+            msg.data.putString("counter", i.toString())
+            mHandler.sendMessage(msg)
             Thread.sleep(2000)
             println("i: $i")
         }
